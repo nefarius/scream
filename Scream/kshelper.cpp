cf28a01b1e4bc67335a69ed1e34738740cadd5bd
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-04-29 21:14:27 +0200

    Update kshelper.cpp

--- Scream/kshelper.cpp
+++ Scream/kshelper.cpp
@@ -31,12 +31,15 @@ Return Value:
     PAGED_CODE();
 
     PWAVEFORMATEX pWfx = NULL;
-    
+
     // If this is a known dataformat extract the waveformat info.
-    if( pDataFormat && (IsEqualGUIDAligned(pDataFormat->MajorFormat, KSDATAFORMAT_TYPE_AUDIO) && (IsEqualGUIDAligned(pDataFormat->Specifier, KSDATAFORMAT_SPECIFIER_WAVEFORMATEX) || IsEqualGUIDAligned(pDataFormat->Specifier, KSDATAFORMAT_SPECIFIER_DSOUND)))) {
+    if (pDataFormat && (IsEqualGUIDAligned(pDataFormat->MajorFormat, KSDATAFORMAT_TYPE_AUDIO) && (
+        IsEqualGUIDAligned(pDataFormat->Specifier, KSDATAFORMAT_SPECIFIER_WAVEFORMATEX) || IsEqualGUIDAligned(
+            pDataFormat->Specifier,
+            KSDATAFORMAT_SPECIFIER_DSOUND)))) {
         pWfx = PWAVEFORMATEX(pDataFormat + 1);
         if (IsEqualGUIDAligned(pDataFormat->Specifier, KSDATAFORMAT_SPECIFIER_DSOUND)) {
-            PKSDSOUND_BUFFERDESC    pwfxds;
+            PKSDSOUND_BUFFERDESC pwfxds;
             pwfxds = PKSDSOUND_BUFFERDESC(pDataFormat + 1);
             pWfx = &pwfxds->WaveFormatEx;
         }
@@ -47,9 +50,9 @@ Return Value:
 
 //-----------------------------------------------------------------------------
 NTSTATUS PropertyHandler_BasicSupport(
-    IN PPCPROPERTY_REQUEST         PropertyRequest,
-    IN ULONG                       Flags,
-    IN DWORD                       PropTypeSetId
+    IN PPCPROPERTY_REQUEST PropertyRequest,
+    IN ULONG               Flags,
+    IN DWORD               PropTypeSetId
 )
 /*++
 Routine Description:
@@ -75,28 +78,31 @@ Return Value:
     if (PropertyRequest->ValueSize >= sizeof(KSPROPERTY_DESCRIPTION)) {
         // if return buffer can hold a KSPROPERTY_DESCRIPTION, return it
         PKSPROPERTY_DESCRIPTION PropDesc = PKSPROPERTY_DESCRIPTION(PropertyRequest->Value);
-        PropDesc->AccessFlags       = Flags;
-        PropDesc->DescriptionSize   = sizeof(KSPROPERTY_DESCRIPTION);
+        PropDesc->AccessFlags = Flags;
+        PropDesc->DescriptionSize = sizeof(KSPROPERTY_DESCRIPTION);
         if (VT_ILLEGAL != PropTypeSetId) {
-            PropDesc->PropTypeSet.Set   = KSPROPTYPESETID_General;
-            PropDesc->PropTypeSet.Id    = PropTypeSetId;
-        } else {
-            PropDesc->PropTypeSet.Set   = GUID_NULL;
-            PropDesc->PropTypeSet.Id    = 0;
+            PropDesc->PropTypeSet.Set = KSPROPTYPESETID_General;
+            PropDesc->PropTypeSet.Id = PropTypeSetId;
+        }
+        else {
+            PropDesc->PropTypeSet.Set = GUID_NULL;
+            PropDesc->PropTypeSet.Id = 0;
         }
         PropDesc->PropTypeSet.Flags = 0;
-        PropDesc->MembersListCount  = 0;
-        PropDesc->Reserved          = 0;
+        PropDesc->MembersListCount = 0;
+        PropDesc->Reserved = 0;
 
         PropertyRequest->ValueSize = sizeof(KSPROPERTY_DESCRIPTION);
         ntStatus = STATUS_SUCCESS;
-    } else if (PropertyRequest->ValueSize >= sizeof(ULONG)) {
+    }
+    else if (PropertyRequest->ValueSize >= sizeof(ULONG)) {
         // if return buffer can hold a ULONG, return the access flags
         *(PULONG(PropertyRequest->Value)) = Flags;
 
         PropertyRequest->ValueSize = sizeof(ULONG);
-        ntStatus = STATUS_SUCCESS;                    
-    } else {
+        ntStatus = STATUS_SUCCESS;
+    }
+    else {
         PropertyRequest->ValueSize = 0;
         ntStatus = STATUS_BUFFER_TOO_SMALL;
     }
@@ -104,40 +110,30 @@ Return Value:
     return ntStatus;
 } // PropertyHandler_BasicSupport
 
-//-----------------------------------------------------------------------------
 NTSTATUS ValidatePropertyParams(
-    IN PPCPROPERTY_REQUEST      PropertyRequest, 
-    IN ULONG                    cbSize,
-    IN ULONG                    cbInstanceSize /* = 0  */
+    IN PPCPROPERTY_REQUEST PropertyRequest,
+    IN ULONG               cbSize,
+    IN ULONG               cbInstanceSize /* = 0  */
 )
-/*++
-Routine Description:
-  Validates property parameters.
-
-Arguments:
-  PropertyRequest - 
-  cbSize -
-  cbInstanceSize -
-
-Return Value:
-  NT status code.
---*/
 {
     PAGED_CODE();
 
     NTSTATUS ntStatus = STATUS_UNSUCCESSFUL;
 
     if (PropertyRequest && cbSize) {
-        if (0 == PropertyRequest->ValueSize)  {
+        if (0 == PropertyRequest->ValueSize) {
             // If the caller is asking for ValueSize.
             PropertyRequest->ValueSize = cbSize;
             ntStatus = STATUS_BUFFER_OVERFLOW;
-        } else if (PropertyRequest->ValueSize < cbSize) {
+        }
+        else if (PropertyRequest->ValueSize < cbSize) {
             // If the caller passed an invalid ValueSize.
             ntStatus = STATUS_BUFFER_TOO_SMALL;
-        } else if (PropertyRequest->InstanceSize < cbInstanceSize) {
+        }
+        else if (PropertyRequest->InstanceSize < cbInstanceSize) {
             ntStatus = STATUS_BUFFER_TOO_SMALL;
-        } else if (PropertyRequest->ValueSize == cbSize) {
+        }
+        else if (PropertyRequest->ValueSize == cbSize) {
             // If all parameters are OK.
             if (PropertyRequest->Value) {
                 ntStatus = STATUS_SUCCESS;
@@ -147,16 +143,15 @@ Return Value:
                 //
             }
         }
-    } else {
+    }
+    else {
         ntStatus = STATUS_INVALID_PARAMETER;
     }
-    
+
     // Clear the ValueSize if unsuccessful.
     if (PropertyRequest && STATUS_SUCCESS != ntStatus && STATUS_BUFFER_OVERFLOW != ntStatus) {
         PropertyRequest->ValueSize = 0;
     }
 
     return ntStatus;
-} // ValidatePropertyParams
-
-
+}
